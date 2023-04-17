c21510b2bb3307752c06c6addea74f5af450b597
Tarek994 <ghali.tarek.123@hotmail.com>
2023-04-17 23:09:25 +0200

    fix(root): fix code style to standard JS

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,15 @@
-const inputInteger = require("..");
+document.documentElement.setAttribute("lang", "en")
+document.title = "Document"
+const meta1 = document.createElement("meta")
+const meta2 = document.createElement("meta")
+meta1.setAttribute("http-equiv", "X-UA-Compatible")
+meta1.setAttribute("content", "IE=Edge")
+meta2.setAttribute("name", "viewport")
+meta2.setAttribute("content", "width=device-width initial-scale=1.0")
+document.head.append(meta1, meta2)
 
-const x = inputInteger();
+const inputInteger = require("..")
 
-document.body.innerHTML = `<h1>${x}</h1>`;
+const x = inputInteger()
+
+document.body.append(x)
