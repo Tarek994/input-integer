32829bb03c1337ee05bbd6dd9b97180734ded0c3
Tarek994 <ghali.tarek.123@hotmail.com>
2023-04-18 03:13:07 +0200

    feat(index.js): create a new element and surround it using a shadow Dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-module.exports = inputInteger;
+module.exports = inputInteger
+
+const sheet = new CSSStyleSheet
+const theme = get_theme()
+sheet.replaceSync(theme)
+
 
 function inputInteger() {
-  const name = "input-integer";
-  return name;
+  const el = document.createElement("div")
+  const shadow = el.attachShadow({ mode: "closed" })
+  shadow.innerHTML = `
+  <input></input>
+  `
+  shadow.adoptedStyleSheets = [sheet]
+  return el
+}
+
+function get_theme(){
+  return`
+   input {
+    padding: 4px 0px;
+    background-color: #123123;
+    border-radius: 8px;
+   }
+   input:focus{
+    background-color:#fff;
+   }
+   `
 }
